216904b07c2d253b05065970ed62adbebb2fa1b7
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-12 21:17:02 -0300

    feat: adicionando nome personalizado de tabela

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,23 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 
+import re
+
+from pydantic import field_validator
+
 class User(SQLModel):
     username: str = Field(min_length=8, max_length=20)
     phone: str = Field(min_length=9, max_length=11)
-    password: str = Field(min_length=6, max_length=12)
+    password: str = Field(min_length=6, max_length=72)
+
+    @field_validator("phone")
+    @classmethod
+    def validate_phone(cls, v):
+        cleaned_v = re.sub(r"[\s\-\(\)]", "", v)
+        if not cleaned_v.is_digit():
+            raise ValueError("Telefone inválido.")
+        return v
+
 
 class UserDb(SQLModel, table=True):
     id: int | None= Field(default=None, primary_key=True)
@@ -14,7 +27,22 @@ class UserDb(SQLModel, table=True):
     is_deleted: bool = Field(default=False)
     created_at: str
 
+    __tablename__ = "users"
+
 class UserPublic(SQLModel):
     id: int | None= Field(default=None, primary_key=True)
     username: str = Field(min_length=8, max_length=20)
     phone: str = Field(min_length=9, max_length=11)
+
+class UserUpdate(SQLModel):
+    username: str | None = Field(default=None, min_length=8, max_length=20)
+    phone: str | None = Field(default=None, min_length=9, max_length=11)
+    password: str | None = Field(default=None, min_length=6, max_length=72)
+
+    @field_validator("phone")
+    @classmethod
+    def validate_phone(cls, v):
+        cleaned_v = re.sub(r"[\s\-\(\)]", "", v)
+        if not cleaned_v.is_digit():
+            raise ValueError("Telefone inválido.")
+        return v
