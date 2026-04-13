bd1e7868b1559e9d1b9423ac8e5710c915f5ae26
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-12 21:37:12 -0300

    feat: adicionando nome personalizado na tabela

--- app/models/friendships.py
+++ app/models/friendships.py
@@ -8,9 +8,11 @@ class StatusEnum(str, Enum):
     ACCEPTED = "Accepted"
     DECLINED = "Declined"
 
-class Friendships(SQLModel, table=True):
+class Friendship(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     requester_id: int = Field(foreign_key="users.id")
     receiver_id: int = Field(foreign_key="users.id")
     status: StatusEnum = Field(default=StatusEnum.PENDING)
     is_deleted: bool = Field(default=False)
+
+    __tablename__ = "friendships"
