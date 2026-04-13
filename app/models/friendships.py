76a14611ffb63efa6a1d38ab7feed61865f1b634
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-12 21:12:41 -0300

    feat: criando tabela de amizades

---
+++ app/models/friendships.py
@@ -0,0 +1,16 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+from enum import Enum
+
+class StatusEnum(str, Enum):
+    PENDING = "Pending"
+    ACCEPTED = "Accepted"
+    DECLINED = "Declined"
+
+class FriendShips(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    requester_id: int = Field(foreign_key="users.id")
+    receiver_id: int = Field(foreign_key="users.id")
+    status: StatusEnum = Field(default=StatusEnum.PENDING)
+    is_deleted: bool = Field(default=False)
