171ce2aad4f7633f0724a222b2f5849f52bfac10
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-12 21:37:35 -0300

    feat: criando rota de delete e patch do usuário

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, Cookie
+from fastapi import APIRouter, Body, Cookie, Depends
 from typing import Annotated
 from dotenv import load_dotenv
 from datetime import datetime, timezone, timedelta
@@ -29,6 +29,11 @@ ALGORITHM = "HS256"
 SECRET_KEY = os.environ["SECRET_KEY"]
 EXPIRE_ACCESS_TOKEN = 30
 
+credentials_exception = HTTPException(
+    detail="Credenciais inválidas",
+    status_code=401
+)
+
 def verify_password(password: str, user: UserDb):
     if not password_hash.verify(password, user.hashed_password):
         raise HTTPException(detail="Credenciais inválidas", status_code=400)
@@ -52,6 +57,19 @@ def verify_existing_user(phone: str, session: SessionDep):
     user = session.exec(select(UserDb).where(UserDb.phone == phone)).first()
     if user:
         raise HTTPException(detail="Já existe um usuário com esse número de telefone!", status_code=400)
+    
+def get_current_user( session: SessionDep, access_token: Annotated[str, Cookie()]):
+    try:
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload["id"]
+        if not user_id:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    user = session.exec(select(UserDb).where(UserDb.id == user_id)).first()
+
+    return user
 
 @router.post("/users/create_user", response_model=UserPublic)
 def create_user(
@@ -61,7 +79,7 @@ def create_user(
     verify_existing_user(phone=user.phone, session=session)
 
     now = datetime.now()
-    user_db = UserDb(username=user.username, hashed_password=password_hash.hash(user.password), phone=user.phone, created_at=now.strftime("%d/%m/%Y"))
+    user_db = UserDb(username=user.username.strip(), hashed_password=password_hash.hash(user.password).strip(), phone=user.phone.strip(), created_at=now.strftime("%d/%m/%Y"))
     session.add(user_db)
     session.commit()
     session.refresh(user_db)
@@ -78,20 +96,39 @@ def login(phone: Annotated[str, Body()], password: Annotated[str, Body()], sessi
     return {"access_token": access_token}
 
 @router.get("/users/me", response_model=UserPublic)
-def get_current_user( session: SessionDep, access_token: str = Cookie()):
-    credentials_exception = HTTPException(
-        detail="Credenciais inválidas",
-        status_code=401
-    )
+def read_users_me(user: Annotated[UserDb, Depends(get_current_user)]):
+    return user 
 
-    try:
-        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload["id"]
-        if not user_id:
-            raise credentials_exception
-    except InvalidTokenError:
+@router.delete("/users/")
+def delete_user(current_user: Annotated[UserDb, Depends(get_current_user)], session: SessionDep):
+    if not current_user:
         raise credentials_exception
-    
-    user = session.exec(select(UserDb).where(UserDb.id == user_id)).first()
 
-    return user
+    current_user.is_deleted == True
+    session.add(current_user)
+    session.commit()
+    session.refresh(current_user)
+
+    return JSONResponse(content={"message": "Usuário deletado com sucesso!"}, status_code=200)
+
+@router.patch("/users/")
+def update_user(current_user: Annotated[UserDb, Depends(get_current_user)], user_update: UserUpdate, session: SessionDep):
+
+    if user_update.username is not None:
+        current_user.username = user_update.username.strip()
+
+    if user_update.phone is not None:
+        current_user.phone = user_update.phone.strip()
+
+    if user_update.password is not None:
+        if not password_hash.verify(user_update.password, current_user.hashed_password):
+            current_user.hashed_password = password_hash.hash(user_update.password.strip())
+        else:
+            raise HTTPException(detail="Use uma senha diferente da atual.", status_code=400)
+        
+    session.add(current_user)
+    session.commit()
+    session.refresh(current_user)
+
+    return JSONResponse(content={"message": "Usuário atualizado com sucesso"}, status_code=200)
+    
