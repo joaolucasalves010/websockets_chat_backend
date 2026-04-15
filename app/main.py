a333532e6818f6c1ae974c41d7bc1bc2c7dd890a
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-15 18:57:01 -0300

    feat: adicionando rota websocket

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-from .routers import user, friendships
+from .routers import user, friendships, websocket
 
 from fastapi.middleware.cors import CORSMiddleware
 
 from .database import create_db_and_tables
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(friendships.router)
+app.include_router(websocket.router)
 
 app.add_middleware(
     CORSMiddleware,
