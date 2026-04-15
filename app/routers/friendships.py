a5998d01f106779ea6e7a03114be98a6b5af09a6
joaolucasalves010 <ylucasslalves@gmail.com>
2026-04-15 15:51:10 -0300

    feat: criando rota de aceitar amizade e listar amizades

--- app/routers/friendships.py
+++ app/routers/friendships.py
@@ -18,15 +18,15 @@ from ..models.friendships import *
 router = APIRouter(tags=["friendships"])
 
 @router.post("/add_friend/{phone}")
-def add_friend(current_user: Annotated[UserDb, Depends(get_current_user)], phone: Annotated[str, Path()], session: SessionDep):
-    if not current_user:
-        raise credentials_exception
-    
+def add_friend(current_user: Annotated[UserDb, Depends(get_current_user)], phone: Annotated[str, Path()], session: SessionDep):    
     receiver = session.exec(select(UserDb).where(UserDb.phone == phone)).first()
 
     if not receiver:
         raise HTTPException(detail="Usuário não encontrado!", status_code=404)
     
+    if receiver.id == current_user.id:
+        raise HTTPException(detail="Você não pode enviar um pedido de amizade para si mesmo.", status_code=400)
+   
     friend_request = Friendship(requester_id=current_user.id, receiver_id=receiver.id)
 
     session.add(friend_request)
@@ -35,11 +35,27 @@ def add_friend(current_user: Annotated[UserDb, Depends(get_current_user)], phone
 
     return JSONResponse(content={"message": "Pedido de amizade enviado com sucesso!"}, status_code=200)
 
+@router.get("/friendships")
+def list_friendships(
+    current_user: Annotated[UserDb, Depends(get_current_user)],
+    session: SessionDep
+):
+    friendships = session.exec(select(Friendship).where(
+        and_(
+            Friendship.status == "Accepted",
+            Friendship.is_deleted == False
+        ),
+        or_(
+            Friendship.requester_id == current_user.id,
+            Friendship.receiver_id == current_user.id
+        )
+    )).all()
+
+    return friendships
+
+
 @router.delete("/delete_friendship/{user_id}")
 def delete_friendship(current_user: Annotated[UserDb, Depends(get_current_user)], user_id: Annotated[int, Path()], session: SessionDep):
-    if not current_user:
-        raise credentials_exception
-    
     user = session.exec(select(UserDb).where(UserDb.id == user_id)).first()
 
     if not user:
@@ -59,13 +75,29 @@ def delete_friendship(current_user: Annotated[UserDb, Depends(get_current_user)]
         )
     )).first()
 
+    if friendship.receiver_id != current_user.id and friendship.requester_id != current_user.id:
+        raise HTTPException(detail="Você não tem permissão para fazer essa chamada.", status_code=401)
+
     if not friendship or friendship.is_deleted:
         raise HTTPException(detail="Amizade não encontrada!", status_code=404)
     
-    friendship.is_deleted == True
+    friendship.is_deleted = True
 
     session.add(friendship)
     session.commit()
     session.refresh(friendship)
 
     return JSONResponse(content={"message": "Amizade deletada com sucesso!"}, status_code=200)
+
+@router.post("/accept_friendhsip/{user_id}")
+def accept_friendship(current_user: Annotated[UserDb, Depends(get_current_user)], user_id: Annotated[int, Path()], session: SessionDep):
+    friendship_request = session.exec(select(Friendship).where(Friendship.receiver_id == current_user.id, Friendship.requester_id == user_id, Friendship.status == "Pending", Friendship.is_deleted == False)).one_or_none()
+
+    if not friendship_request:
+        return HTTPException(detail="Pedido de amizade não encontrado!", status_code=404)
+    
+    friendship_request.status = "Accepted"
+    session.commit()
+    session.refresh(friendship_request)
+
+    return JSONResponse(content={"message": "Pedido de amizade aceito."}, status_code=200)
